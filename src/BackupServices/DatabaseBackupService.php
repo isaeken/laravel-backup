1aa800abe7761ef18bf73e69fdee4cd0020732fe
İsa Eken <hello@isaeken.com.tr>
2022-01-24 00:11:36 +0300

    wip

---
+++ src/BackupServices/DatabaseBackupService.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace IsaEken\LaravelBackup\BackupServices;
+
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
+use IsaEken\LaravelBackup\BackupServices\BackupService as BaseBackupService;
+
+class DatabaseBackupService extends BaseBackupService implements \IsaEken\LaravelBackup\Contracts\BackupService
+{
+    protected string $name = 'database';
+
+    public string|null $connection = null;
+
+    public function setConnection(string $connection): static
+    {
+        $this->connection = $connection;
+        return $this;
+    }
+
+    public function getConnection(): array
+    {
+        $connection = $this->connection ?? config('database.default');
+        return config('database.connections')[$connection];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function run(): void
+    {
+        $driver = $this->getConnection()['driver'];
+
+        if ($driver === 'sqlite') {
+            $this->sqlite();
+        }
+    }
+
+    private function sqlite(): void
+    {
+        $databasePath = $this->getConnection()['database'];
+        $databaseName = Str::beforeLast(basename($databasePath), '.');
+        @File::copy($databasePath, $backupFile = $this->temporaryDirectory->path($databaseName . '.sqlite'));
+
+        $this
+            ->getCompressor()
+            ->setSource($backupFile)
+            ->setDestination($this->temporaryDirectory->path(now()->format('Y-m-d-H-i-s') . '.sqlite'));
+
+        if ($this->getCompressor()->run()) {
+            $this->outputFile = $this->getCompressor()->getDestination();
+            $this->success = true;
+            $this->success('Backup generated: ' . $this->outputFile);
+        } else {
+            $this->error('Compression failed!');
+        }
+    }
+}
--- src/Tasks/FakeBackup.php
+++ src/Tasks/FakeBackup.php
@@ -2,12 +2,14 @@
 
 namespace IsaEken\LaravelBackup\Tasks;
 
+use IsaEken\LaravelBackup\BackupServices\DatabaseBackupService;
 use IsaEken\LaravelBackup\BackupServices\StorageBackupService;
 use IsaEken\LaravelBackup\Storages\FakeBackupStorage;
 
 class FakeBackup extends Backup
 {
     public array $services = [
+        DatabaseBackupService::class,
         StorageBackupService::class,
     ];
 
