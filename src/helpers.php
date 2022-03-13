1efa082eb4528fd577cf56809e3aeba4bf476a6f
İsa Eken <hello@isaeken.com.tr>
2022-03-13 23:35:07 +0300

    add helpers

--- src/helpers.php
+++ src/helpers.php
@@ -1,3 +1,27 @@
 <?php
 
-// ...
+use Carbon\Carbon;
+
+if (!function_exists('humanReadableFileSize')) {
+    function humanReadableFileSize(float $bytes): string
+    {
+        $units = ['B', 'KB', 'MB', 'GB', 'TB'];
+
+        if ($bytes < 1) {
+            return '0 '.$units[1];
+        }
+
+        for ($i = 0; $bytes > 1024; $i++) {
+            $bytes /= 1024;
+        }
+
+        return round($bytes, 2).' '.$units[$i];
+    }
+}
+
+if (!function_exists('ageInDays')) {
+    function ageInDays(Carbon $date): string
+    {
+        return number_format(round($date->diffInMinutes() / (24 * 60), 2), 2).' ('.$date->diffForHumans().')';
+    }
+}
