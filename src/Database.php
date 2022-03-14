2c0f97719b9434175233e67bda2b519e93ae261f
İsa Eken <hello@isaeken.com.tr>
2022-03-14 22:59:27 +0300

    wip

---
+++ src/Database.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace IsaEken\LaravelBackup;
+
+use Illuminate\Support\Collection;
+
+class Database
+{
+    private Collection $backups;
+
+    public function __construct()
+    {
+        $this->backups = new Collection();
+    }
+
+    /**
+     * Get backups.
+     *
+     * @return Collection<\IsaEken\LaravelBackup\Models\Backup>
+     */
+    public function backups(): Collection
+    {
+        return $this->backups;
+    }
+
+    /**
+     * Load database.
+     *
+     * @return $this
+     */
+    public function load(): static
+    {
+        $backups = @json_decode(@file_get_contents(config('backup.database')) ?? []);
+        foreach ($backups as $backup) {
+            $this
+                ->backups()
+                ->add(
+                    new \IsaEken\LaravelBackup\Models\Backup((array) $backup)
+                );
+        }
+
+        return $this;
+    }
+
+    /**
+     * Save database.
+     *
+     * @return $this
+     */
+    public function save(): static
+    {
+        @file_put_contents(config('backup.database'), $this->backups()->toJson());
+        return $this;
+    }
+}
---
+++ src/Models/Backup.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace IsaEken\LaravelBackup\Models;
+
+use Carbon\Carbon;
+use Illuminate\Contracts\Filesystem\Filesystem;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property Filesystem $filesystem
+ * @property string $driver
+ * @property string $filename
+ * @property int $size
+ * @property Carbon $date
+ */
+class Backup extends Model
+{
+    public function setFilesystem(Filesystem $filesystem): static
+    {
+        $this->setAttribute('filesystem', $filesystem);
+        return $this;
+    }
+
+    public function getFilesystem(): Filesystem
+    {
+        return $this->getAttribute('filesystem');
+    }
+
+    public function setDriver(string $driver): static
+    {
+        $this->setAttribute('driver', $driver);
+        return $this;
+    }
+
+    public function getDriver(): string
+    {
+        return $this->getAttribute('driver');
+    }
+
+    public function setFilename(string $filename): static
+    {
+        $this->setAttribute('filename', $filename);
+        return $this;
+    }
+
+    public function getFilename(): string
+    {
+        return $this->getAttribute('filename');
+    }
+
+    public function setSize(int $size): static
+    {
+        $this->setAttribute('size', $size);
+        return $this;
+    }
+
+    public function getSize(): int
+    {
+        return $this->getAttribute('size');
+    }
+
+    public function setDate(Carbon $date): static
+    {
+        $this->setAttribute('date', $date);
+        return $this;
+    }
+
+    public function getDate(): Carbon
+    {
+        return $this->getAttribute('date');
+    }
+}
