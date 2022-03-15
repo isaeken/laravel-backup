f0f767c36bf5869aaa310e80145237fc42f2ec32
İsa Eken <hello@isaeken.com.tr>
2022-03-16 00:10:38 +0300

    wip

--- src/Backup.php
+++ src/Backup.php
@@ -2,7 +2,7 @@
 
 namespace IsaEken\LaravelBackup;
 
-use Illuminate\Contracts\Filesystem\Filesystem;
+use Illuminate\Support\Str;
 use IsaEken\LaravelBackup\Contracts\Backup\Manager;
 use IsaEken\LaravelBackup\Contracts\Backup\Service;
 use IsaEken\LaravelBackup\Contracts\HasBackupServices;
@@ -19,62 +19,128 @@ class Backup implements Manager, HasLogger, HasBackupServices, HasBackupStorages
     use Traits\HasLogger;
     use Traits\HasNotifications;
 
+    protected function makeFilename(string $filename, Service $service): string
+    {
+        $pattern = config('backup.pattern', 'backup_:app_name_:service_name_:datetime.:extension');
+        $extension = Str::afterLast($filename, '.');
+
+        $replaces = [
+            'filename' => $filename,
+            'extension' => $extension,
+            'app_name' => config('app.name', 'Laravel'),
+            'service_name' => $service->getName(),
+            'date' => date('Y-m-d'),
+            'time' => date('H-i-s'),
+            'datetime' => date('Y-m-d-H-i-s'),
+        ];
+
+        $filename = Str::of($pattern);
+        foreach ($replaces as $key => $value) {
+            $filename = $filename->replace(":$key", $value);
+        }
+
+        return $filename->value();
+    }
+
+    /**
+     * @return array<Service>
+     */
+    protected function createBackups(): array
+    {
+        $backupServices = [];
+
+        foreach ($this->getBackupServices() as $backupService) {
+            $this->debug(trans('Running backup service: :service', [
+                'service' => $backupService->getName(),
+            ]));
+
+            if ($backupService instanceof HasLogger && $this->getOutput() !== null) {
+                $this->debug(trans('Setting logger ":logger" for service: :service', [
+                    'logger' => $this->getOutput()::class,
+                    'service' => $backupService->getName(),
+                ]));
+
+                $backupService->setOutput($this->getOutput());
+            }
+
+            if ($backupService instanceof HasPassword) {
+                $this->debug(trans('Setting password "******" for service: service', [
+                    'service' => $backupService->getName(),
+                ]));
+
+                $backupService->setPassword($this->getPassword());
+            }
+
+            $this->debug(trans('Backup generating...'));
+            $backupService->run();
+
+            if ($backupService->isSuccessful()) {
+                $this->debug(trans('Backup generated.'));
+
+                if ($backupService->getOutputFile() !== null) {
+                    $this->debug(trans('Output file: :file', [
+                        'file' => $backupService->getOutputFile(),
+                    ]));
+
+                    $backupServices[] = $backupService;
+                }
+            } else {
+                $this->error(trans('Backup is cannot be created!'));
+            }
+        }
+
+        return $backupServices;
+    }
+
+    /**
+     * @param  array<Service>  $backupServices
+     */
+    protected function storeBackups(array $backupServices)
+    {
+        foreach ($backupServices as $backupService) {
+            foreach ($this->getBackupStorages() as $driver => $storage) {
+                $filename = $this->makeFilename(basename($backupService->getOutputFile()), $backupService);
+
+                $this->debug(trans('Saving backup ":service" to ":filename" with using driver: :driver', [
+                    'service' => $backupService->getName(),
+                    'filename' => $filename,
+                    'driver' => $driver,
+                ]));
+
+                if ($storage->put($filename, file_get_contents($backupService->getOutputFile()))) {
+                    $model = config('backup.model', Models\Backup::class)();
+                    $model->fill([
+                        'filesystem' => $storage,
+                        'driver' => $driver,
+                        'filename' => $filename,
+                        'size' => $storage->size($filename),
+                        'date' => now(),
+                    ]);
+                    $model->save();
+                    $this->debug(trans('Backup saved successfully.'));
+                } else {
+                    $this->error(trans('Backup ":service" cannot be saved to ":filename" with using driver: ":driver"',
+                        [
+                            'service' => $backupService->getName(),
+                            'filename' => $filename,
+                            'driver' => $driver,
+                        ]));
+                }
+            }
+        }
+    }
+
     /**
      * @inheritDoc
      */
     public function run(): void
     {
-        $this->info('Backup is started...');
-        $backups = [];
+        $this->info(trans('Backup is started...'));
+        $backupServices = $this->createBackups();
 
-        /** @var Service $service */
-        foreach ($this->getBackupServices() as $service) {
-            $this->debug('Running backup service: '.$service->getName());
+        $this->debug(trans('Saving backups to storages...'));
+        $this->storeBackups($backupServices);
 
-            if ($service instanceof HasLogger && $this->getOutput() !== null) {
-                $this->debug('Setting service logger: '.$this->getOutput()::class);
-                $service->setOutput($this->getOutput());
-            }
-
-            if ($service instanceof HasPassword) {
-                $this->debug('Setting service password: '.$this->getPassword());
-                $service->setPassword($this->getPassword());
-            }
-
-            $this->debug('Backup generating...');
-            $service->run();
-
-            if ($service->isSuccessful()) {
-                $this->debug('Backup generated.');
-
-                if ($service->getOutputFile() !== null) {
-                    $this->debug('Output file: '.$service->getOutputFile());
-                    $backups[] = $service;
-                }
-            } else {
-                $this->error('Backup is cannot be created!');
-            }
-        }
-
-        $this->info('Saving backups to storages...');
-
-        foreach ($backups as $backup) {
-            /** @var Filesystem $storage */
-            foreach ($this->getBackupStorages() as $storage) {
-                $storageClass = $storage::class;
-
-                $this->info("Saving backup '{$backup->getName()}' with using driver: '$storageClass'");
-                if ($storage->put(
-                    config('backup.prefix').basename($backup->getOutputFile()),
-                    file_get_contents($backup->getOutputFile())
-                )) {
-                    $this->debug('Backup saved successfully.');
-                } else {
-                    $this->error('Backup cannot be saved with using driver: '.$storageClass);
-                }
-            }
-        }
-
-        $this->success('Backup completed.');
+        $this->success(trans('Backup completed.'));
     }
 }
