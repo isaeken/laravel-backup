14fc8da6f0036f4e6c3483e7b3deb4d3a0100700
İsa Eken <hello@isaeken.com.tr>
2022-03-27 23:56:23 +0300

    add get methods for storages and services

--- src/BackupServiceProvider.php
+++ src/BackupServiceProvider.php
@@ -2,8 +2,12 @@
 
 namespace IsaEken\LaravelBackup;
 
+use Illuminate\Contracts\Filesystem\Filesystem;
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Filesystem\FilesystemManager;
 use IsaEken\LaravelBackup\Commands\BackupCommand;
 use IsaEken\LaravelBackup\Commands\ListCommand;
+use IsaEken\LaravelBackup\Contracts\Backup\Service;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -29,5 +33,92 @@ class BackupServiceProvider extends PackageServiceProvider
     public function packageRegistered()
     {
         // ...
+    }
+
+    /**
+     * @param  string  $name
+     * @return Service|null
+     */
+    public function getService(string $name): Service|null
+    {
+        $services = config('backup.services', []) ?? [];
+
+        /** @var Service $service */
+        foreach ($services as $service) {
+            if ($service instanceof $name) {
+                return new $service();
+            }
+
+            if (($service = new $service())->getName() == $name) {
+                return $service;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * @return array<Service>
+     */
+    public function getServices(): array
+    {
+        $services = collect();
+        $makeServiceCollection = function ($array) {
+            return collect($array)->map(function ($service) {
+                return new $service();
+            });
+        };
+
+        if (func_num_args() === 0 || (func_num_args() === 1 && func_get_arg(0) === '*')) {
+            return $makeServiceCollection(config('backup.services', []) ?? [])->toArray();
+        }
+
+        foreach (func_get_args() as $service) {
+            if (is_array($service) || $service instanceof Arrayable) {
+                $services = $services->merge($makeServiceCollection($service));
+            } else {
+                $services = $services->merge([$makeServiceCollection($service)]);
+            }
+        }
+
+        return $services->toArray();
+    }
+
+    /**
+     * @param  string  $name
+     * @return Filesystem
+     */
+    public function getStorage(string $name): Filesystem
+    {
+        /** @var FilesystemManager $filesystemManager */
+        $filesystemManager = $this->app->factory('filesystem');
+        return $filesystemManager->drive($name);
+    }
+
+    /**
+     * @return array<Filesystem>
+     */
+    public function getStorages(): array
+    {
+        $storages = collect();
+        $makeStorageCollection = function ($array) {
+            return collect($array)->mapWithKeys(function ($storage) {
+                return [$storage => $this->getStorage($storage)];
+            });
+        };
+
+        if (func_num_args() === 0 || (func_num_args() === 1 && func_get_arg(0) === '*')) {
+            return $makeStorageCollection(config('backup.storages', []) ?? [])->toArray();
+        }
+
+        foreach (func_get_args() as $driver) {
+            if (is_array($driver) || $driver instanceof Arrayable) {
+                $storages = $storages->merge($makeStorageCollection($driver));
+            } else {
+                $storages = $storages->merge([$makeStorageCollection($driver)]);
+            }
+        }
+
+        return $storages->toArray();
     }
 }
